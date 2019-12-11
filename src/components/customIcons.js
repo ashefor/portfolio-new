12be79233eced9ab15da5a269103ae57440c0898
Shredder <mashefor@gmail.com>
2019-12-11 23:37:24 +0100

    feat: add custom icon component

---
+++ src/components/customIcons.js
@@ -0,0 +1,114 @@
+import React from 'react';
+import {Icon} from 'antd'
+
+const bulmaSvg = ()=> (
+    <svg version="1.1" id="Layer_1" width="20px" height="20px" xmlns="http://www.w3.org/2000/svg" xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+	  viewBox="0 0 256 373" enableBackground="new 0 0 256 373" xml="preserve">
+<g>
+	<polygon fill="#00D1B2" points="34.031,237.438 51.117,117.841 119.458,49.5 204.884,134.926 153.628,186.182 221.969,254.523 
+		119.458,322.863 	"/>
+</g>
+</svg>
+)
+
+const antSvg = () => (
+    <svg width="20px" height="20px" viewBox="0 0 200 200" version="1.1" xmlns="http://www.w3.org/2000/svg" xlink="http://www.w3.org/1999/xlink">
+    <defs>
+        <linearGradient x1="62.1023273%" y1="0%" x2="108.19718%" y2="37.8635764%" id="linearGradient-1">
+            <stop stopColor="#4285EB" offset="0%"></stop>
+            <stop stopColor="#2EC7FF" offset="100%"></stop>
+        </linearGradient>
+        <linearGradient x1="69.644116%" y1="0%" x2="54.0428975%" y2="108.456714%" id="linearGradient-2">
+            <stop stopColor="#29CDFF" offset="0%"></stop>
+            <stop stopColor="#148EFF" offset="37.8600687%"></stop>
+            <stop stopColor="#0A60FF" offset="100%"></stop>
+        </linearGradient>
+        <linearGradient x1="69.6908165%" y1="-12.9743587%" x2="16.7228981%" y2="117.391248%" id="linearGradient-3">
+            <stop stopColor="#FA816E" offset="0%"></stop>
+            <stop stopColor="#F74A5C" offset="41.472606%"></stop>
+            <stop stopColor="#F51D2C" offset="100%"></stop>
+        </linearGradient>
+        <linearGradient x1="68.1279872%" y1="-35.6905737%" x2="30.4400914%" y2="114.942679%" id="linearGradient-4">
+            <stop stopColor="#FA8E7D" offset="0%"></stop>
+            <stop stopColor="#F74A5C" offset="51.2635191%"></stop>
+            <stop stopColor="#F51D2C" offset="100%"></stop>
+        </linearGradient>
+    </defs>
+    <g id="Page-1" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+        <g id="logo" transform="translate(-20.000000, -20.000000)">
+            <g id="Group-28-Copy-5" transform="translate(20.000000, 20.000000)">
+                <g id="Group-27-Copy-3">
+                    <g id="Group-25" fillRule="nonzero">
+                        <g id="2">
+                            <path d="M91.5880863,4.17652823 L4.17996544,91.5127728 C-0.519240605,96.2081146 -0.519240605,103.791885 4.17996544,108.487227 L91.5880863,195.823472 C96.2872923,200.518814 103.877304,200.518814 108.57651,195.823472 L145.225487,159.204632 C149.433969,154.999611 149.433969,148.181924 145.225487,143.976903 C141.017005,139.771881 134.193707,139.771881 129.985225,143.976903 L102.20193,171.737352 C101.032305,172.906015 99.2571609,172.906015 98.0875359,171.737352 L28.285908,101.993122 C27.1162831,100.824459 27.1162831,99.050775 28.285908,97.8821118 L98.0875359,28.1378823 C99.2571609,26.9692191 101.032305,26.9692191 102.20193,28.1378823 L129.985225,55.8983314 C134.193707,60.1033528 141.017005,60.1033528 145.225487,55.8983314 C149.433969,51.69331 149.433969,44.8756232 145.225487,40.6706018 L108.58055,4.05574592 C103.862049,-0.537986846 96.2692618,-0.500797906 91.5880863,4.17652823 Z" id="Shape" fill="url(#linearGradient-1)"></path>
+                            <path d="M91.5880863,4.17652823 L4.17996544,91.5127728 C-0.519240605,96.2081146 -0.519240605,103.791885 4.17996544,108.487227 L91.5880863,195.823472 C96.2872923,200.518814 103.877304,200.518814 108.57651,195.823472 L145.225487,159.204632 C149.433969,154.999611 149.433969,148.181924 145.225487,143.976903 C141.017005,139.771881 134.193707,139.771881 129.985225,143.976903 L102.20193,171.737352 C101.032305,172.906015 99.2571609,172.906015 98.0875359,171.737352 L28.285908,101.993122 C27.1162831,100.824459 27.1162831,99.050775 28.285908,97.8821118 L98.0875359,28.1378823 C100.999864,25.6271836 105.751642,20.541824 112.729652,19.3524487 C117.915585,18.4685261 123.585219,20.4140239 129.738554,25.1889424 C125.624663,21.0784292 118.571995,14.0340304 108.58055,4.05574592 C103.862049,-0.537986846 96.2692618,-0.500797906 91.5880863,4.17652823 Z" id="Shape" fill="url(#linearGradient-2)"></path>
+                        </g>
+                        <path d="M153.685633,135.854579 C157.894115,140.0596 164.717412,140.0596 168.925894,135.854579 L195.959977,108.842726 C200.659183,104.147384 200.659183,96.5636133 195.960527,91.8688194 L168.690777,64.7181159 C164.472332,60.5180858 157.646868,60.5241425 153.435895,64.7316526 C149.227413,68.936674 149.227413,75.7543607 153.435895,79.9593821 L171.854035,98.3623765 C173.02366,99.5310396 173.02366,101.304724 171.854035,102.473387 L153.685633,120.626849 C149.47715,124.83187 149.47715,131.649557 153.685633,135.854579 Z" id="Shape" fill="url(#linearGradient-3)"></path>
+                    </g>
+                    <ellipse id="Combined-Shape" fill="url(#linearGradient-4)" cx="100.519339" cy="100.436681" rx="23.6001926" ry="23.580786"></ellipse>
+                </g>
+            </g>
+        </g>
+    </g>
+</svg>
+)
+
+const firebaseSvg = () => (
+    <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+    width="20px" height="20px" viewBox="0 0 256 351" enableBackground="new 0 0 256 351" xml="preserve">
+<filter  width="200%" height="200%" x="-50%" y="-50%" id="filter-4" filterUnits="objectBoundingBox">
+	<feGaussianBlur  stdDeviation="3.5" result="shadowBlurInner1" in="SourceAlpha"></feGaussianBlur>
+	<feOffset  result="shadowOffsetInner1" dx="1" dy="-9" in="shadowBlurInner1"></feOffset>
+	<feComposite  result="shadowInnerInner1" k2="-1" in2="SourceAlpha" operator="arithmetic" k3="1" in="shadowOffsetInner1">
+		</feComposite>
+	<feColorMatrix  type="matrix" values="0 0 0 0 0   0 0 0 0 0   0 0 0 0 0  0 0 0 0.09 0" in="shadowInnerInner1"></feColorMatrix>
+</filter>
+<filter  width="200%" height="200%" x="-50%" y="-50%" id="filter-2" filterUnits="objectBoundingBox">
+	<feGaussianBlur  stdDeviation="17.5" result="shadowBlurInner1" in="SourceAlpha"></feGaussianBlur>
+	<feOffset  result="shadowOffsetInner1" dx="0" dy="0" in="shadowBlurInner1"></feOffset>
+	<feComposite  result="shadowInnerInner1" k2="-1" in2="SourceAlpha" operator="arithmetic" k3="1" in="shadowOffsetInner1">
+		</feComposite>
+	<feColorMatrix  type="matrix" values="0 0 0 0 0   0 0 0 0 0   0 0 0 0 0  0 0 0 0.06 0" in="shadowInnerInner1"></feColorMatrix>
+</filter>
+<g>
+	<path fill="#FFC24A" d="M47.349,243.066l1.335-1.871l63.176-119.873l0.133-1.269L84.145,67.742
+		c-2.333-4.382-8.902-3.274-9.667,1.631L47.349,243.066z"/>
+	<g>
+		<g>
+			<path id="path-1" fill="#FFA712" d="M48.137,241.641l1.011-1.97l62.514-118.618L83.882,68.529
+				c-2.309-4.337-8.171-3.231-8.929,1.623L48.137,241.641z"/>
+		</g>
+		<g filter="url(#filter-2)">
+			<path id="path-1_2_" d="M48.137,241.641l1.011-1.97l62.514-118.618L83.882,68.529c-2.309-4.337-8.171-3.231-8.929,1.623
+				L48.137,241.641z"/>
+		</g>
+	</g>
+	<path fill="#F4BD62" d="M132.296,159.622l20.735-21.237L132.29,98.789c-1.97-3.748-7.467-3.759-9.415,0l-11.082,21.134v1.8
+		L132.296,159.622z"/>
+	<g>
+		<g>
+			<path id="path-3" fill="#FFA50E" d="M131.926,158.736l20.159-20.646l-20.159-38.386c-1.914-3.644-6.564-4.026-8.458-0.372
+				l-11.141,21.462l-0.333,1.097L131.926,158.736z"/>
+		</g>
+		<g filter="url(#filter-4)">
+			<path id="path-3_2_" d="M131.926,158.736l20.159-20.646l-20.159-38.386c-1.914-3.644-6.564-4.026-8.458-0.372l-11.141,21.462
+				l-0.333,1.097L131.926,158.736z"/>
+		</g>
+	</g>
+	<polygon fill="#F6820C" points="47.349,243.066 47.954,242.458 50.153,241.563 130.994,161.024 132.017,158.236 111.852,119.808 	
+		"/>
+	<path fill="#FDE068" d="M134.886,283.684l73.162-40.803l-20.894-128.67c-0.653-4.026-5.592-5.618-8.473-2.73L47.349,243.066
+		l72.742,40.615C124.688,286.247,130.286,286.248,134.886,283.684"/>
+	<path fill="#FCCA3F" d="M207.392,242.539l-20.733-127.694c-0.649-3.995-4.756-5.648-7.615-2.783L48.16,242.816l71.943,40.213
+		c4.562,2.546,10.119,2.547,14.682,0.003L207.392,242.539z"/>
+	<path fill="#EEAB37" d="M134.886,282.482c-4.6,2.563-10.198,2.563-14.794-0.005l-72.157-40.03l-0.586,0.619l72.743,40.615
+		c4.597,2.565,10.195,2.566,14.794,0.002l73.162-40.803l-0.18-1.102L134.886,282.482z"/>
+</g>
+</svg>
+)
+
+const BulmaIcon = props => <Icon component={bulmaSvg} {...props}/>;
+const AntIcon = props => <Icon component={antSvg} {...props} />;
+const FirebaseIcon = props => <Icon component={firebaseSvg} {...props} />
+
+export default {BulmaIcon, AntIcon, FirebaseIcon}
